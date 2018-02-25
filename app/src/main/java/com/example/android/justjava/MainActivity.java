0c16ca2ee74e59990729566fe1aa3aec7da552ca
Rahul Pancholi <Rahul2pancholi@users.noreply.github.com>
2018-02-25 16:55:01 +0530

    code improvement

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -21,17 +22,21 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
     int quantity = 0;
+    TextView quantityTextView;
+    TextView priceTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+         priceTextView = (TextView) findViewById(R.id.price_text_view);
     }
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+
         priceTextView.setText(message);
     }
     /**
@@ -48,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+
         quantityTextView.setText("" + number);
     }
 
@@ -56,13 +61,20 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
     public void decrement(View view) {
         quantity = quantity - 1;
-        display(quantity);
+        if(quantity<0)
+        {
+            Toast.makeText(this,"Not negative",Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            display(quantity);
+        }
 
     }
 
